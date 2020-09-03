13c7b98d10e0a7d35c34ddeda30dcdf06377fd17
Janis Kacens <janis.kacens@28stone.com>
2020-09-03 19:37:51 +0300

    Module 6 Done

--- APM/src/app/app.module.ts
+++ APM/src/app/app.module.ts
@@ -2,17 +2,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { ProductListComponent } from './products/product-list.component'
-import { CommonModule } from '@angular/common'; 
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
     AppComponent,
     ProductListComponent,
-    
   ],
   imports: [
     BrowserModule,
     CommonModule,
+    FormsModule,
+    
   ],
   bootstrap: [AppComponent]
 })
