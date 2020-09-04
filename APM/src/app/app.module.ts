41499adec54e9108258d1df74f8a913e18c8b2c7
Janis Kacens <janis.kacens@28stone.com>
2020-09-04 16:19:32 +0300

    Module 7

--- APM/src/app/app.module.ts
+++ APM/src/app/app.module.ts
@@ -4,17 +4,19 @@ import { AppComponent } from './app.component';
 import { ProductListComponent } from './products/product-list.component'
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
+import { ConvertToSpacesPipe } from './shared/convert-to-spaces.pipe'
 
 @NgModule({
   declarations: [
     AppComponent,
     ProductListComponent,
+    ConvertToSpacesPipe,
   ],
   imports: [
     BrowserModule,
     CommonModule,
     FormsModule,
-    
+
   ],
   bootstrap: [AppComponent]
 })
